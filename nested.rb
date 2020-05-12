59e68d4a3027579305d8877eb500b9681120f4bf
michael thomas <michael.thomas2014@gmail.com>
2020-05-12 07:28:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -80,6 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+  programmer_hash[:yukihiro_matsumoto]
 
     
 end
